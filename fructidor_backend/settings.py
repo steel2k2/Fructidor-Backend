5b6f300897849089a6abc3b63426daa454922400
steel2k2 <alkeides2k2@gmail.com>
2026-05-24 20:22:32 -0400

    Fix: configurar ALLOWED_HOSTS dinámico desde variables de entorno

--- fructidor_backend/settings.py
+++ fructidor_backend/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-h-zqcemzfv$5a^633r!&mn065^yk%40bhze2=3mdy3+ww#rka&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
